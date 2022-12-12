caaf8d445fad72f1b89303def06377adae10334b
nickisainez <nickiaasainez@gmail.com>
2022-12-12 01:36:27 -0500

    eeeee

--- miportofolio/settings.py
+++ miportofolio/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR/'static']
+STATICFILES_DIRS = [BASE_DIR/'miportofolio/static']
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/' 
