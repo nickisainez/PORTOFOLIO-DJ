393e68c05855fa7286abdfa8b8b3e99f71f71096
nickisainez <nickiaasainez@gmail.com>
2022-12-12 01:00:36 -0500

    statics2

--- miportofolio/settings.py
+++ miportofolio/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'myportofilio/staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'miportofolio/static'),)
 
 MEDIA_URL = '/media/' 
